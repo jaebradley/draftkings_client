2c31ee0a5d55a5a16ed5f877d0474b1bd99eff81
jaebradley <jae.b.bradley@gmail.com>
2016-11-17 05:36:42 -0500

    date translator test

---
+++ tests/test_dateTranslator.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+from draftkings_client.data.translators.date_translator import DateTranslator
+
+
+class TestDateTranslator(TestCase):
+    def test_translate(self):
+        timestamp = DateTranslator.translate(date_string="/Date(1479258000000)/")
+        self.assertIsNotNone(timestamp)
+        self.assertEqual(timestamp, 1479258000000)
+
