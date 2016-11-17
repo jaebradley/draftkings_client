bbc283613580744c91a8be31940eaade8c3fa618
jaebradley <jae.b.bradley@gmail.com>
2016-11-17 10:27:05 -0500

    add translator test

---
+++ tests/test_availablePlayersTranslator.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+
+from draftkings_client.data.translators.available_players_translator import AvailablePlayersTranslator
+from draftkings_client.data.models.available_players import AvailablePlayers
+
+
+class TestAvailablePlayersTranslator(TestCase):
+
+    def test_translation(self):
+        response = {
+            'playerList': [],
+            'teamList': {}
+        }
+
+        expected_translation = AvailablePlayers(player_list=[], team_series_list=[])
+        translation = AvailablePlayersTranslator.translate(response=response)
+        self.assertEqual(translation, expected_translation)
+
+    def test_field_validation(self):
+        missing_player_list_response = {
+            'teamList': {}
+        }
+
+        self.assertRaises(KeyError, AvailablePlayersTranslator.translate, response=missing_player_list_response)
+
+        missing_team_list_response = {
+            'playerList': []
+        }
+
+        self.assertRaises(KeyError, AvailablePlayersTranslator.translate, response=missing_team_list_response)
