de082a395fbebda5befc5b050e494f2d6186d4a9
jaebradley <jae.b.bradley@gmail.com>
2016-11-22 23:12:29 -0500

    fix import issue

--- draftkings_client/draftkings_client.py
+++ draftkings_client/draftkings_client.py
@@ -3,7 +3,7 @@ import requests
 from data.models.league import League
 from data.translators.available_players_translator import AvailablePlayersTranslator
 from data.translators.contests_response_translator import ContestsResponseTranslator
-from draftkings_client.data.utilities.url_builder import UrlBuilder
+from data.utilities.url_builder import UrlBuilder
 
 
 class DraftkingsClient:
