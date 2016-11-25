9cad85fe4f4278ac9b7771d0acb50226dadd3614
jaebradley <jae.b.bradley@gmail.com>
2016-11-25 12:59:02 -0500

    changed league to sport

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,12 +1,12 @@
 from unittest import TestCase
 
 from draft_kings_client.draft_kings_client import DraftKingsClient
-from draft_kings_client.data.models.league import League
+from draft_kings_client.data.models.sport import Sport
 
 
 class TestClient(TestCase):
     def test_get_contests(self):
-        result = DraftKingsClient.get_contests(League.nba)
+        result = DraftKingsClient.get_contests(Sport.nba)
         self.assertIsNotNone(result)
 
     def test_get_available_players(self):
