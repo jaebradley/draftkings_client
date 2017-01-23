6c8183136cf1bd46f029a58bb22fb7f7d8e7e372
jbradley <jbradley@hubspot.com>
2017-01-23 11:45:08 -0500

    add position model

---
+++ draft_kings_client/data/models/position.py
@@ -0,0 +1,46 @@
+from enum import Enum
+
+from sport import Sport
+
+
+class Position(Enum):
+    point_guard = {
+        'sport': Sport.nba,
+        'abbreviation': 'PG',
+        'name': 'Point Guard'
+    }
+    shooting_guard = {
+        'sport': Sport.nba,
+        'abbreviation': 'SG',
+        'name': 'Shooting Guard'
+    }
+    small_forward = {
+        'sport': Sport.nba,
+        'abbreviation': 'SF',
+        'name': 'Small Forward'
+    }
+    power_forward = {
+        'sport': Sport.nba,
+        'abbreviation': 'PF',
+        'name': 'Power Forward'
+    }
+    center = {
+        'sport': Sport.nba,
+        'abbreviation': 'C',
+        'name': 'Center'
+    }
+
+    @staticmethod
+    def value_of(sport, abbreviations):
+        return [Position.value_of(sport=sport, abbreviations=abbreviation) for abbreviation in abbreviations]
+
+    @staticmethod
+    def value_of(sport, abbreviation):
+        assert isinstance(sport, Sport)
+        assert isinstance(abbreviation, basestring)
+
+        for position in Position:
+            if position.value['sport'] == sport and position.value['abbreviation'] == abbreviation:
+                return position
+
+        raise ValueError('Unable to identify position for sport: %s and abbreviation: %s', sport, abbreviation)
