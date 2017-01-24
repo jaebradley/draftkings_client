1f928cdd96719c7bc59ba52ecdc685947f2f2ed5
jaebradley <jae.b.bradley@gmail.com>
2017-01-24 02:33:21 -0500

    date translator

--- draft_kings_client/data/translators/date_translator.py
+++ draft_kings_client/data/translators/date_translator.py
@@ -1,7 +1,11 @@
+from datetime import datetime
+import pytz
+
+
 class DateTranslator:
     def __init__(self):
         pass
 
     @staticmethod
     def translate(date_string):
-        return long(date_string[6:-2])
+        return datetime.fromtimestamp(timestamp=long(date_string[6:-2]), tz=pytz.utc)
