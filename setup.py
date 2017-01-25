1fbc960636182a27a6b782074b18cacc80069e39
jaebradley <jae.b.bradley@gmail.com>
2017-01-24 21:58:07 -0500

    updated download url

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
   author = 'Jae Bradley',
   author_email = 'jae.b.bradley@gmail.com',
   url = 'https://github.com/jaebradley/draftkings_client', # use the URL to the github repo
-  download_url = 'https://github.com/jaebradley/draftkings_client/tarball/0.1', # I'll explain this in a second
+  download_url = 'https://github.com/jaebradley/draftkings_client/tarball/0.2', # I'll explain this in a second
   keywords = ['sports', 'dfs', 'draftkings'], # arbitrary keywords
   classifiers = [],
 )
