79152618ec20ffc7bc35846f930b2e694d7a950d
jaebradley <jae.b.bradley@gmail.com>
2018-03-28 08:29:31 -0700

    update readme and setup

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 
 from setuptools import setup, find_packages
 setup(
-  name = 'draft_kings',
-  packages = find_packages(exclude=['tests*']),
+  name='draft_kings',
+  packages=find_packages(exclude=['tests*']),
   install_requires=['requests', 'enum34'],
-  version = '0.8',
-  description = 'A DraftKings client',
-  author = 'Jae Bradley',
-  author_email = 'jae.b.bradley@gmail.com',
-  url = 'https://github.com/jaebradley/draftkings_client', # use the URL to the github repo
-  download_url = 'https://github.com/jaebradley/draftkings_client/tarball/0.2', # I'll explain this in a second
-  keywords = ['sports', 'dfs', 'draftkings'], # arbitrary keywords
-  classifiers = [],
+  version='0.9',
+  description='A DraftKings client',
+  author='Jae Bradley',
+  author_email='jae.b.bradley@gmail.com',
+  url='https://github.com/jaebradley/draftkings_client',
+  download_url='https://github.com/jaebradley/draftkings_client/tarball/0.2',
+  keywords=['sports', 'dfs', 'draftkings'],
+  classifiers=[],
 )
