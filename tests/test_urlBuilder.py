cae3edd11380ee4ef002d00d3db5ad5c3f0a0d37
Jae Bradley <jae.b.bradley@gmail.com>
2018-11-07 21:20:51 -0800

    Refactor url building, fix requirements, and modify imports in client file

--- tests/test_urlBuilder.py
+++ tests/test_urlBuilder.py
@@ -1,11 +1,27 @@
 from unittest import TestCase
 
-from draft_kings_client.data.utilities.url_builder import UrlBuilder
+from draft_kings_client.urls import CONTESTS_URL, AVAILABLE_PLAYERS_URL, COUNTRIES_URL, draft_group_url, regions_url, \
+    contest_url, draftables_url
 
 
-class TestUrlBuilder(TestCase):
+class TestUrls(TestCase):
+    def test_contests_url(self):
+        self.assertEqual(CONTESTS_URL, "https://www.draftkings.com/lobby/getcontests")
+
+    def test_available_players_url(self):
+        self.assertEqual(AVAILABLE_PLAYERS_URL, "https://www.draftkings.com/lineup/getavailableplayers")
+
+    def test_countries_url(self):
+        self.assertEqual(COUNTRIES_URL, "https://api.draftkings.com/addresses/v1/countries/")
+
+    def test_draft_group_url(self):
+        self.assertEqual(draft_group_url("jaebaebae"), "https://api.draftkings.com/draftgroups/v1/jaebaebae")
+
+    def test_regions_url(self):
+        self.assertEqual(regions_url("jaebaebae"), "https://api.draftkings.com/addresses/v1/countries/jaebaebae/regions")
+
     def test_contest_url(self):
-        self.assertEqual(UrlBuilder.get_contest_url(), 'https://www.draftkings.com/lobby/getcontests')
+        self.assertEqual(contest_url("jaebaebae"), "https://api.draftkings.com/contests/v1/contests/jaebaebae")
 
-    def test_get_available_players_url(self):
-        self.assertEqual(UrlBuilder.get_available_players_url(), 'https://www.draftkings.com/lineup/getavailableplayers')
+    def test_draftables_url(self):
+        self.assertEqual(draftables_url("jaebaebae"), "https://api.draftkings.com/draftgroups/v1/jaebaebae/draftables")
