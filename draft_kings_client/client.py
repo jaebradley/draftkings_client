6fd60b936b0f8f2814b040d03bce3ba22ce0f95f
Jae Bradley <jae.b.bradley@gmail.com>
2018-11-17 23:47:37 -0800

    update client file

---
+++ draft_kings_client/client.py
@@ -0,0 +1,83 @@
+import requests
+
+from draft_kings_client.data import Sport
+from draft_kings_client.response_translators import translate_players, translate_contests
+from draft_kings_client import urls
+
+sports = {
+    Sport.NFL: "NFL",
+    Sport.NHL: "NHL",
+    Sport.NBA: "NBA",
+    Sport.CFL: "CFL",
+    Sport.COLLEGE_FOOTBALL: "CFB",
+    Sport.MIXED_MARTIAL_ARTS: "MMA",
+    Sport.NASCAR: "NAS",
+    Sport.SOCCER: "SOC",
+    Sport.EUROLEAGUE_BASKETBALL: "EL",
+    Sport.MLB: "MLB",
+    Sport.TENNIS: "TEN",
+    Sport.LEAGUE_OF_LEGENDS: "LOL",
+}
+
+
+def contests(sport):
+    response = requests.get(url=urls.CONTESTS_URL,
+                            params={'sport': sports[sport]})
+
+    response.raise_for_status()
+
+    return translate_contests(response=response.json())
+
+
+def available_players(draft_group_id):
+    response = requests.get(url=urls.AVAILABLE_PLAYERS_URL,
+                            params={'draftGroupId': draft_group_id})
+
+    response.raise_for_status()
+
+    return translate_players(response=response.json())
+
+
+def draft_group_details(draft_group_id):
+    response = requests.get(url=urls.draft_group_url(draft_group_id),
+                            params={'format': 'json'})
+
+    response.raise_for_status()
+
+    return response.json()
+
+
+def countries():
+    response = requests.get(url=urls.COUNTRIES_URL,
+                            params={'format': 'json'})
+
+    response.raise_for_status()
+
+    return response.json()
+
+
+def regions(country_code):
+    response = requests.get(url=urls.regions_url(country_code),
+                            params={'format': 'json'})
+
+    response.raise_for_status()
+
+    return response.json()
+
+
+def contest_details(contest_id):
+    response = requests.get(url=urls.contest_url(contest_id),
+                            params={'format': 'json'})
+
+    response.raise_for_status()
+
+    return response.json()
+
+
+def draftables(draft_group_id):
+    response = requests.get(url=urls.draftables_url(draft_group_id),
+                            params={'format': 'json'})
+
+    response.raise_for_status()
+
+    return response.json()
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,34 +1,35 @@
 from unittest import TestCase
 
 from draft_kings_client.data import Sport
-from draft_kings_client.draft_kings_client import DraftKingsClient
+from draft_kings_client.client import contests, available_players, draft_group_details, countries, regions, \
+    contest_details, draftables
 
 
 class TestClient(TestCase):
     def test_get_contests(self):
-        result = DraftKingsClient.get_contests(Sport.NBA)
+        result = contests(Sport.NBA)
         self.assertIsNotNone(result)
 
     def test_get_available_players(self):
-        result = DraftKingsClient.get_available_players(draft_group_id=11513)
+        result = available_players(draft_group_id=11513)
         self.assertIsNotNone(result)
 
     def test_get_draft_group_details(self):
-        response = DraftKingsClient.get_draft_group_details(draft_group_id=11513)
+        response = draft_group_details(draft_group_id=11513)
         self.assertIsNotNone(response)
 
     def test_get_countries(self):
-        response = DraftKingsClient.get_countries()
+        response = countries()
         self.assertIsNotNone(response)
 
     def test_get_regions(self):
-        response = DraftKingsClient.get_regions(country_code='US')
+        response = regions(country_code='US')
         self.assertIsNotNone(response)
 
     def test_get_contest_details(self):
-        response = DraftKingsClient.get_contest_details(contest_id=54639629)
+        response = contest_details(contest_id=54639629)
         self.assertIsNotNone(response)
 
     def test_get_draftables(self):
-        response = DraftKingsClient.get_draftables(draft_group_id=18186)
+        response = draftables(draft_group_id=18186)
         self.assertIsNotNone(response)
