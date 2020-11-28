e65a0616df0bbe03e8af2ff066b4de19a3510051
Jae Bradley <jae.b.bradley@gmail.com>
2020-11-27 21:36:08 -0500

    Update draft group output schema and introduce custom datetime-aware serializers (#77)

--- draft_kings/output/schema/draft_group.py
+++ draft_kings/output/schema/draft_group.py
@@ -1,54 +1,63 @@
-from marshmallow import Schema, fields, EXCLUDE
+from marshmallow import Schema, fields, post_load
+
+from draft_kings.output.objects.draft_group import ContestDetails, StartTimeDetails, LeagueDetails, GameDetails, \
+    DraftGroupDetails
+from draft_kings.output.schema.fields import CustomAwareDateTime
 
 
 class ContestDetailsSchema(Schema):
-    class Meta:
-        unknown = EXCLUDE
-
-    game_type = fields.Str(missing=None)
+    game_type_description = fields.Str(missing=None)
     type_id = fields.Int(missing=None)
 
+    @post_load
+    def make_contest_details(self, data, **kwargs):
+        return ContestDetails(**data)
 
-class StartsAtDetailsSchema(Schema):
-    class Meta:
-        unknown = EXCLUDE
 
-    maximum = fields.AwareDateTime(missing=None)
-    minimum = fields.AwareDateTime(missing=None)
+class StartTimeDetailsSchema(Schema):
+    maximum = CustomAwareDateTime(missing=None)
+    minimum = CustomAwareDateTime(missing=None)
     type_description = fields.Str(missing=None)
+
+    @post_load
+    def make_start_time_details(self, data, **kwargs):
+        return StartTimeDetails(**data)
 
 
 class LeagueDetailsSchema(Schema):
-    class Meta:
-        unknown = EXCLUDE
-
     abbreviation = fields.Str(missing=None)
     league_id = fields.Int(missing=None)
     name = fields.Str(missing=None)
 
+    @post_load
+    def make_league_details(self, data, **kwargs):
+        return LeagueDetails(**data)
+
 
 class GameDetailsSchema(Schema):
-    class Meta:
-        unknown = EXCLUDE
-
     away_team_id = fields.Int(missing=None)
     description = fields.Str(missing=None)
     game_id = fields.Int(missing=None)
     home_team_id = fields.Int(missing=None)
     location = fields.Str(missing=None)
     name = fields.Str(missing=None)
-    starts_at = fields.AwareDateTime(missing=None)
-    status = fields.Str(missing=None)
+    starts_at = CustomAwareDateTime(missing=None)
+    status_description = fields.Str(missing=None)
+
+    @post_load
+    def make_game_details(self, data, **kwargs):
+        return GameDetails(**data)
 
 
-class DraftGroupSchema(Schema):
-    class Meta:
-        unknown = EXCLUDE
-
-    contest_details = fields.Nested(ContestDetailsSchema, required=True)
+class DraftGroupDetailsSchema(Schema):
+    contest_details = fields.Nested(ContestDetailsSchema(), required=True)
     draft_group_id = fields.Int(missing=None)
-    games = fields.List(fields.Nested(GameDetailsSchema, required=True), missing=[])
-    leagues = fields.List(fields.Nested(LeagueDetailsSchema, required=True), missing=None)
+    games = fields.List(fields.Nested(GameDetailsSchema(), required=True), missing=[])
+    leagues = fields.List(fields.Nested(LeagueDetailsSchema(), required=True), missing=[])
     sport = fields.Str(missing=None)
-    starts_at = fields.Nested(StartsAtDetailsSchema, required=True)
-    state = fields.Str(missing=None)
+    start_time_details = fields.Nested(StartTimeDetailsSchema(), required=True)
+    state_description = fields.Str(missing=None)
+
+    @post_load
+    def make_draft_group_details(self, data, **kwargs):
+        return DraftGroupDetails(**data)
