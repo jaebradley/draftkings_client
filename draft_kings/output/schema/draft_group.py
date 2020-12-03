749dcd07834d08196818a2f93f3db6d2042df3a5
Jae Bradley <jae.b.bradley@gmail.com>
2020-12-02 22:47:19 -0500

    Add more serialization and deserialization tests

--- draft_kings/output/schema/draft_group.py
+++ draft_kings/output/schema/draft_group.py
@@ -1,5 +1,7 @@
 from marshmallow import Schema, fields, post_load
+from marshmallow_enum import EnumField
 
+from draft_kings.data import Sport
 from draft_kings.output.objects.draft_group import ContestDetails, StartTimeDetails, LeagueDetails, GameDetails, \
     DraftGroupDetails
 from draft_kings.output.schema.fields import CustomAwareDateTime
@@ -54,7 +56,7 @@ class DraftGroupDetailsSchema(Schema):
     draft_group_id = fields.Int(allow_none=True, required=True)
     games = fields.List(fields.Nested(GameDetailsSchema, required=True), required=True)
     leagues = fields.List(fields.Nested(LeagueDetailsSchema, required=True), required=True)
-    sport = fields.Str(allow_none=True, required=True)
+    sport = EnumField(Sport, allow_none=True, required=True, by_value=True)
     start_time_details = fields.Nested(StartTimeDetailsSchema, required=True)
     state_description = fields.Str(allow_none=True, required=True)
 
