d0dd44048520c7cc8e8149b13648adfc9799dc9e
Jae Bradley <jae.b.bradley@gmail.com>
2020-12-15 14:06:07 -0500

    Fix breaking tests for available players

--- tests/integration/client/test_available_players.py
+++ tests/integration/client/test_available_players.py
@@ -12,41 +12,41 @@ from tests.config import ROOT_DIRECTORY
 
 class TestNBAAvailablePlayers(TestCase):
     def setUp(self) -> None:
-        self.result = Client().available_players(draft_group_id=11513)
+        self.result = Client().available_players(draft_group_id=42463)
 
     def test_get_available_players(self):
         self.assertIsNotNone(self.result)
 
     def test_nba_multiple_players(self):
-        self.assertEqual(len(self.result.players), 150)
+        self.assertEqual(len(self.result.players), 75)
 
     def test_nba_multiple_teams(self):
-        self.assertEqual(len(self.result.team_series), 5)
+        self.assertEqual(len(self.result.team_series), 2)
 
     def test_nba_player_fields(self):
         self.assertEqual(
             PlayerDetails(
                 draft_details=DraftDetails(
                     is_draftable=True,
-                    salary=float(10300),
-                    starts_at=datetime.datetime(2016, 11, 16, 0, 0, 0, tzinfo=datetime.timezone.utc)
+                    salary=float(10000),
+                    starts_at=None
                 ),
                 exceptional_messages=[],
-                first_name="LeBron",
-                jersey_number=23,
-                last_name="James",
-                player_id=214152,
-                points_per_game=55.7,
+                first_name="Anthony",
+                jersey_number=3,
+                last_name="Davis",
+                player_id=603096,
+                points_per_game=50.3,
                 position_details=PositionDetails(
-                    name="SF/PF",
-                    position_id=27
+                    name="PF/C",
+                    position_id=28
                 ),
-                team_id=5,
+                team_id=13,
                 team_series_details=PlayerTeamSeriesDetails(
-                    away_team_id=28,
-                    home_team_id=5,
-                    opposition_rank=15,
-                    team_series_id=5479720
+                    away_team_id=12,
+                    home_team_id=13,
+                    opposition_rank=13,
+                    team_series_id=5713406
                 )
             ),
             self.result.players[0]
@@ -56,111 +56,21 @@ class TestNBAAvailablePlayers(TestCase):
         self.assertListEqual(
             [
                 TeamSeriesDetails(
-                    away_team_id=28,
-                    home_team_id=5,
-                    starts_at=datetime.datetime(2016, 11, 16, 0, 0, 0, tzinfo=datetime.timezone.utc),
-                    status_description="Final",
-                    team_series_id=5479720,
-                    weather_description=None
-                ),
-                TeamSeriesDetails(
-                    away_team_id=4,
-                    home_team_id=22,
-                    starts_at=datetime.datetime(2016, 11, 16, 3, 0, 0, tzinfo=datetime.timezone.utc),
-                    status_description="Final",
-                    team_series_id=5479968,
-                    weather_description=None
-                ),
-                TeamSeriesDetails(
-                    away_team_id=1,
-                    home_team_id=14,
-                    starts_at=datetime.datetime(2016, 11, 16, 0, 30, 0, tzinfo=datetime.timezone.utc),
-                    status_description="Final",
-                    team_series_id=5479637,
-                    weather_description=None
-                ),
-                TeamSeriesDetails(
-                    away_team_id=5312,
-                    home_team_id=16,
-                    starts_at=datetime.datetime(2016, 11, 16, 1, 0, 0, tzinfo=datetime.timezone.utc),
-                    status_description="Final",
-                    team_series_id=5479280,
-                    weather_description=None
-                ),
-                TeamSeriesDetails(
-                    away_team_id=17,
+                    away_team_id=12,
                     home_team_id=13,
-                    starts_at=datetime.datetime(2016, 11, 16, 3, 30, 0, tzinfo=datetime.timezone.utc),
-                    status_description="Final",
-                    team_series_id=5480294,
-                    weather_description=None
-                )
-            ],
-            self.result.team_series
-        )
-
-
-class TestLeagueOfLegendsAvailablePlayers(TestCase):
-    def setUp(self) -> None:
-        self.result = Client().available_players(draft_group_id=26691)
-
-    def test_get_available_players_league_of_legends(self):
-        self.assertIsNotNone(self.result)
-
-    def test_lol_multiple_players(self):
-        self.assertEqual(28, len(self.result.players))
-
-    def test_lol_multiple_teams(self):
-        self.assertEqual(2, len(self.result.team_series))
-
-    def test_lol_player_fields(self):
-        self.assertEqual(
-            PlayerDetails(
-                draft_details=DraftDetails(
-                    is_draftable=True,
-                    salary=float(9000),
-                    starts_at=datetime.datetime(2019, 5, 1, 9, 0, 0, tzinfo=datetime.timezone.utc)
-                ),
-                exceptional_messages=[],
-                first_name="",
-                jersey_number=None,
-                last_name="BigKoro",
-                player_id=1283,
-                points_per_game=float(0),
-                position_details=PositionDetails(
-                    name="ADC",
-                    position_id=155
-                ),
-                team_id=62492,
-                team_series_details=PlayerTeamSeriesDetails(
-                    away_team_id=62492,
-                    home_team_id=156847,
-                    opposition_rank=None,
-                    team_series_id=5613734
-                )
-            ),
-            self.result.players[0]
-        )
-
-    def test_lol_team_series_fields(self):
-        self.assertListEqual(
-            [
-                TeamSeriesDetails(
-                    away_team_id=62492,
-                    home_team_id=156847,
-                    starts_at=datetime.datetime(2019, 5, 1, 9, 0, 0, 0, tzinfo=datetime.timezone.utc),
-                    status_description="Final",
-                    team_series_id=5613734,
+                    starts_at=datetime.datetime(2020, 12, 23, 3, 0, 0, tzinfo=datetime.timezone.utc),
+                    status_description="Pre-Game",
+                    team_series_id=5713406,
                     weather_description=None
                 ),
                 TeamSeriesDetails(
-                    away_team_id=58492,
-                    home_team_id=45073,
-                    starts_at=datetime.datetime(2019, 5, 1, 9, 55, 0, 0, tzinfo=datetime.timezone.utc),
-                    status_description="Final",
-                    team_series_id=5613735,
+                    away_team_id=9,
+                    home_team_id=17,
+                    starts_at=datetime.datetime(2020, 12, 23, 0, 0, 0, tzinfo=datetime.timezone.utc),
+                    status_description="Pre-Game",
+                    team_series_id=5713400,
                     weather_description=None
-                )
+                ),
             ],
             self.result.team_series
         )
